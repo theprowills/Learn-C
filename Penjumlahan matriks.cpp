e36d1aa0001a921b9b222fef0ef0fab950c17954
theprowills <70465283+theprowills@users.noreply.github.com>
2020-08-30 16:45:53 +0700

    Create Penjumlahan matriks.cpp

---
+++ Penjumlahan matriks.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+	int p, l;
+	scanf("%d %d",&l,&p);
+	
+    int arr1[l][p]={},
+        arr2[l][p]={},
+        arr3[l][p]={};
+
+    for (int i=0;i<l;i++)
+	{
+        for (int j=0;j<p;j++)
+		{
+            scanf("%d", &arr1[i][j]);
+		}
+    }
+    
+    printf("\n");
+    for (int i=0;i<l;i++)
+	{
+        for (int j=0;j<p;j++)
+		{
+            scanf("%d", &arr2[i][j]);
+		}
+    }
+
+    for (int i=0;i<l;i++)
+	{
+    	
+        for (int j=0;j<p;j++)
+		{
+            int a = arr1[i][j]+arr2[i][j];
+            printf("%d ", a);
+		}
+        printf("\n");
+    }
+    return 0;
+}
