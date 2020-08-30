4b59061c35e59fcd15188bc9d4c7e32c6a8d148e
theprowills <70465283+theprowills@users.noreply.github.com>
2020-08-30 17:24:03 +0700

    Create Binary search.cpp

---
+++ Binary search.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int binarySearch ( int *arr, int left, int right,int target){
+	while (left <=right)
+	{
+		int mid = (left + right)/2;
+		if (arr[mid] < target)
+			{
+				left = mid + 1;
+			}
+		else if (arr[mid] > target)
+			{right = mid -1;} 
+		else{return mid;}
+	}
+	return -1;
+}
+
+int main (){
+	int n= 10,target;
+	int arr[10] = {2, 3, 5, 7, 9, 10, 11, 12, 14};
+	scanf("%d",&target);
+	printf("%d urutan ke-%d\n", target, binarySearch(arr,0,n-1,target)+1);
+	return 0;
+}
